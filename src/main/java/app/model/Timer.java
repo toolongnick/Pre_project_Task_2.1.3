9388f7af49e9c2bacd52ba41d0a7290d2b9ec12c
Sergey Tashkin <toolongnick@mail.ru>
2021-12-15 13:59:31 +0300

    adding Dog & Timer singelton

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,10 @@
 package app.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
